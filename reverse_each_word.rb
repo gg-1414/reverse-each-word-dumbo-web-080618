4c59fe533e84adeca47639eac506fff30652932d
Gina Lee <ginalee1114@gmail.com>
2018-07-24 16:55:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   reversed_phrase = []
   phrase.each do |word|
-    reversed_phrase << word.reverse
+    reversed_phrase << word.reverse!
   end 
   reversed_phrase
 end 
