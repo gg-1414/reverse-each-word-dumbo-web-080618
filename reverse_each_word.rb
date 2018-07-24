fefa7a907cda70a5a0cf11308524a982747dbb24
Gina Lee <ginalee1114@gmail.com>
2018-07-24 17:14:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(phrase)
   reversed_phrase = phrase.split.collect do |word|
     word.reverse
   end
-  reversed_phrase
+  reversed_phrase.join
 end
 
 # def reverse_each_word(phrase)
